af346196828964441981050bb17df5761a50dedc
Jason Watson <jason.jw@live.co.uk>
2020-09-08 11:59:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
 from setuptools import setup, find_packages
 
-PACKAGENAME = "spefit"
-DESCRIPTION = "Fitting of Single Photoelectron Spectra"
-AUTHOR = "Jason J Watson"
-AUTHOR_EMAIL = "jason.watson@desy.de"
-VERSION = "1.0.0"
-
 setup(
-    name=PACKAGENAME,
+    name="spefit",
     packages=find_packages(),
-    version=VERSION,
-    description=DESCRIPTION,
-    license='BSD3',
+    version="1.0.0",
+    description="Fitting of Single Photoelectron Spectra",
+    license="BSD3",
     install_requires=[
-        'scipy',
-        'numpy',
-        'matplotlib',
-        'tqdm',
-        'numba>=0.50.1',
-        'iminuit>=1.4.9',
+        "scipy",
+        "numpy",
+        "matplotlib",
+        "tqdm",
+        "numba>=0.50.1",
+        "iminuit>=1.4.9",
     ],
-    setup_requires=['pytest-runner', ],
-    tests_require=['pytest', ],
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL
+    python_requires="~=3.6",
+    setup_requires=["pytest-runner",],
+    tests_require=["pytest",],
+    author="Jason J Watson",
+    author_email="jason.watson@desy.de",
+    url="https://github.com/watsonjj/spefit",
 )
